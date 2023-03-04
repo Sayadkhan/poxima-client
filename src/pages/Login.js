b2a83e752ab8ae28dee5320dced2431c1fc66ff6
Sayadkhan <abdullahalsayad03@gmail.com>
2023-03-05 02:39:05 +0600

    login and signup added

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,24 @@
 import React, { useState } from "react";
+import { useLogin } from "../hooks/useLogin";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const { Login, error, loading } = useLogin();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    // login user
+    await Login(email, password);
+  };
+
   return (
-    <form className="login-form flex flex-col gap-5 py-20 mx-auto max-w-sm">
+    <form
+      onSubmit={handleLogin}
+      className="login-form flex flex-col gap-5 py-20 mx-auto max-w-sm"
+    >
       <h2 className="text-4xl font-medium text-sky-400 mb-10">Login</h2>
       <div className="form-control flex flex-col gap-2">
         <label
@@ -47,6 +60,12 @@ const Login = () => {
       >
         Login
       </button>
+      {error && (
+        <p className="bg-rose-500/20 rounded-lg p-5 text-red-500 border border-red-500">
+          {" "}
+          {error}
+        </p>
+      )}
     </form>
   );
 };
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,11 +1,25 @@
 import { useState } from "react";
 
+import { useSignup } from "../hooks/useSignup";
+
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const { Signup, error, loading } = useSignup();
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    // signup user
+    await Signup(email, password);
+  };
+
   return (
-    <form className="login-form flex flex-col gap-5 py-20 mx-auto max-w-sm">
+    <form
+      onSubmit={handleSignup}
+      className="login-form flex flex-col gap-5 py-20 mx-auto max-w-sm"
+    >
       <h2 className="text-4xl font-medium text-sky-400 mb-10">Signup</h2>
       <div className="form-control flex flex-col gap-2">
         <label
@@ -40,13 +54,20 @@ const Signup = () => {
           className="bg-transparent border border-stone-500 py-3 px-5 rounded-xl outline-none focus:border-sky-400 duration-300"
         />
       </div>
-
       <button
+        disabled={loading}
         type="submit"
         className="bg-sky-400 text-slate-900 py-3 rounded-xl hover:bg-sky-500 duration-300 mt-4"
       >
         Signup
       </button>
+
+      {error && (
+        <p className="bg-rose-500/20 rounded-lg p-5 text-red-500 border border-red-500">
+          {" "}
+          {error}
+        </p>
+      )}
     </form>
   );
 };
